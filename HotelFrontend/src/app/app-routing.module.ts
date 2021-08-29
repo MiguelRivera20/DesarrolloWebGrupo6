b4f739bae99db9fdddccb03b02bbc92da127bb4f
JorgeLuisJP <89712962+JorgeLuisJP@users.noreply.github.com>
2021-08-29 03:56:51 -0500

    avance1
    avance 1

--- HotelFrontend/src/app/app-routing.module.ts
+++ HotelFrontend/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContactenosComponent } from "./pages/contactenos/contactenos.component";
+import { DetallesComponent } from "./pages/habitaciones/detalles/detalles.component";
+import { HabitacionesComponent } from "./pages/habitaciones/habitaciones.component";
 
 import { HomeComponent } from "./pages/home/home.component";
+import { ReservasComponent } from "./pages/reservas/reservas.component";
+import { ServiciosComponent } from "./pages/servicios/servicios.component";
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'habitaciones', component: HomeComponent },
-    { path: 'habitaciones/detalles', component: HomeComponent },
-    { path: 'reservas', component: HomeComponent },
-    { path: 'servicios', component: HomeComponent },
-    { path: 'contactenos', component: HomeComponent },
+    { path: 'habitaciones', component: HabitacionesComponent },
+    { path: 'habitaciones/detalles', component: DetallesComponent },
+    { path: 'reservas', component: ReservasComponent },
+    { path: 'servicios', component: ServiciosComponent },
+    { path: 'contactenos', component: ContactenosComponent },
     {path: '', redirectTo: 'home',pathMatch: 'full'}
+    
 ];
 
 @NgModule({
