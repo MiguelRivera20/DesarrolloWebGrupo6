a027e772c44ba63b6a858b2291d1a34ab601727f
MiguelRivera20 <89713301+MiguelRivera20@users.noreply.github.com>
2021-08-30 18:42:35 -0500

    Reserva

--- HotelAdministradorFrontend/src/app/app-routing.module.ts
+++ HotelAdministradorFrontend/src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+
 import { InicioComponent } from "./pages/inicio/inicio.component";
 import { RegistroDetalleHabitacionComponent } from "./pages/registro-detalle-habitacion/registro-detalle-habitacion.component";
-import { RegistroReservaComponent } from "./pages/reserva/registro-reserva/registro-reserva.component";
+import { ReservaComponent } from "./pages/reserva/reserva.component";
 import { RegistroUsuariosComponent } from "./pages/usuarios/registro-usuarios/registro-usuarios.component";
 import { UsuariosComponent } from "./pages/usuarios/usuarios.component";
+import { CheckinComponent } from "./pages/checkin/checkin.component";
+import { RegistroReservaComponent } from "./pages/reserva/registro-reserva/registro-reserva.component";
+
 
 const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
     { path: 'usuarios', component: UsuariosComponent },
     { path: 'usuarios/registro-usuario', component: RegistroUsuariosComponent },
     { path: 'Habitaciones/detalle-habitacion', component: RegistroDetalleHabitacionComponent },
-    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    { path:"Control_Habitaciones/Reserva", component: ReservaComponent},
+    { path:'Control_Habitaciones/Check_in', component:CheckinComponent},
+    {path: 'Control_Habitaciones/Reserva/Registro_Reserva', component:RegistroReservaComponent}
+    
     
 ];
 
