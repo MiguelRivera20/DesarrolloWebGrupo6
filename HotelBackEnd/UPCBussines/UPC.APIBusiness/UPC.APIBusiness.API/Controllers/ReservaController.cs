e792c2dfd5331fc4b027cb92ded44faf365c3fd2
JorgeLuisJP <89712962+JorgeLuisJP@users.noreply.github.com>
2021-09-27 22:03:47 -0500

    JORGE AVANCE - RESERVA
    JORGE AVANCE - RESERVA

---
+++ HotelBackEnd/UPCBussines/UPC.APIBusiness/UPC.APIBusiness.API/Controllers/ReservaController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBContext;
+using Microsoft.AspNetCore.Authorization;
+using DBEntity;
+
+namespace UPC.Business.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/reserva")]
+    public class ReservaController : Controller
+    {
+
+        protected readonly IReservaRepository _ReservaRepository;
+        public ReservaController(IReservaRepository ReservaRepository)
+        {
+            _ReservaRepository = ReservaRepository;
+        }
+        //<sumary>
+
+        [Produces("application/json")]
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("getReserva")]
+        public ActionResult GetReserva(int coReser, int coClient)
+        {
+            ReservaEntity reser = new ReservaEntity();
+            reser.Co_Reserva = coReser;
+            reser.Co_Cliente = coClient;
+            var ret = _ReservaRepository.GetReserva(reser);
+            return Json(ret);
+        }
+
+
+        [Produces("application/json")]
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("insert")]
+        public ActionResult Insert(ReservaEntity reserva)
+        {
+
+            var ret = _ReservaRepository.ReservaPersona(reserva);
+            return Json(ret);
+        }
+
+        /*
+        [Produces("application/json")]
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("getproject")]
+        public ActionResult GetProject(int id)
+        {
+            var ret = _ProjectRepository.GetProject(id);
+            return Json(ret);
+        }*/
+
+
+    }
+}
---
+++ HotelBackEnd/UPCBussines/UPC.APIBusiness/UPC.APIBusiness.DBContext/Interface/IReservaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DBEntity;
+
+namespace DBContext
+{
+    public interface IReservaRepository
+    {
+        BaseResponse ReservaPersona(ReservaEntity reserva);
+
+        BaseResponse GetReserva(ReservaEntity reserva);
+    }
+}
